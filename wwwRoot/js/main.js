2740ce5f88e8617d9bbee8537bcefb6f25b041e5
John R. Sippy <john.sippy@gyro.com>
2013-04-25 22:10:29 -0500

    All static content and layout complete (incl. cross-browser support).

--- wwwRoot/js/main.js
+++ wwwRoot/js/main.js
@@ -1,1 +1,19 @@
+// ====== Layout Helpers ======
 
+function prettyBullets(){
+	$('ul.prettyBullets').each(function(){
+		$(this).find('li').addClass('prettyBullets').prepend('<span class="prettyBullet">&bull;</span>&nbsp;');
+	});
+}
+
+
+// ====== CAROUSEL ======
+
+//TODO - write a simple image carousel
+
+
+// ====== RUNTIME ======
+
+$(document).ready(function(){
+	prettyBullets();
+});
